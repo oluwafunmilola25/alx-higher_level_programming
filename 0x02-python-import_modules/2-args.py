684cc6ced2c5794eca5e8161558104595bcf9b16
oluwafunmilola25 <137732606+oluwafunmilola25@users.noreply.github.com>
2023-12-01 18:28:15 +0100

    fixed all files to compile

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = len(sys.argv) - 1
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    length = len(sys.argv) - 1
+
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(length))
+
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, sys.argv[1]))
+
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        print("{} arguments:".format(length))
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
-    
+    from sys import argv
+
+    length = len(argv)
+    total_sum = 0
+
+    for i in range(1, length):
+        total_sum += int(argv[i])
+    print("{}".format(total_sum))
